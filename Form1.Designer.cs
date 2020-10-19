a650738c0503b424cc3f4897aed301959e64e5fb
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2020-10-19 09:22:49 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace combobox
+{
+    partial class ComboBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmbIzbor = new System.Windows.Forms.ComboBox();
+            this.btnPoruka = new System.Windows.Forms.Button();
+            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.btnIzbrisi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmbIzbor
+            // 
+            this.CmbIzbor.FormattingEnabled = true;
+            this.CmbIzbor.Items.AddRange(new object[] {
+            "",
+            "Auto",
+            "Vetura",
+            "Automobil"});
+            this.CmbIzbor.Location = new System.Drawing.Point(176, 102);
+            this.CmbIzbor.Name = "CmbIzbor";
+            this.CmbIzbor.Size = new System.Drawing.Size(121, 21);
+            this.CmbIzbor.TabIndex = 0;
+            this.CmbIzbor.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnPoruka
+            // 
+            this.btnPoruka.Location = new System.Drawing.Point(490, 379);
+            this.btnPoruka.Name = "btnPoruka";
+            this.btnPoruka.Size = new System.Drawing.Size(227, 23);
+            this.btnPoruka.TabIndex = 1;
+            this.btnPoruka.Text = "Prikaži poruku";
+            this.btnPoruka.UseVisualStyleBackColor = true;
+            this.btnPoruka.Click += new System.EventHandler(this.btnPoruka_Click);
+            // 
+            // txtIspis
+            // 
+            this.txtIspis.Location = new System.Drawing.Point(176, 173);
+            this.txtIspis.Name = "txtIspis";
+            this.txtIspis.Size = new System.Drawing.Size(121, 20);
+            this.txtIspis.TabIndex = 2;
+            // 
+            // btnIzbrisi
+            // 
+            this.btnIzbrisi.Location = new System.Drawing.Point(490, 333);
+            this.btnIzbrisi.Name = "btnIzbrisi";
+            this.btnIzbrisi.Size = new System.Drawing.Size(227, 23);
+            this.btnIzbrisi.TabIndex = 3;
+            this.btnIzbrisi.Text = "Izbriši Poruku";
+            this.btnIzbrisi.UseVisualStyleBackColor = true;
+            this.btnIzbrisi.Click += new System.EventHandler(this.btnIzbrisi_Click);
+            // 
+            // ComboBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIzbrisi);
+            this.Controls.Add(this.txtIspis);
+            this.Controls.Add(this.btnPoruka);
+            this.Controls.Add(this.CmbIzbor);
+            this.Name = "ComboBox";
+            this.Text = "ComboBoxVjezba";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CmbIzbor;
+        private System.Windows.Forms.Button btnPoruka;
+        private System.Windows.Forms.TextBox txtIspis;
+        private System.Windows.Forms.Button btnIzbrisi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combobox
+{
+    public partial class ComboBox : Form
+    {
+        public ComboBox()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtIspis.Text = CmbIzbor.SelectedItem.ToString();
+        }
+
+        private void btnIzbrisi_Click(object sender, EventArgs e)
+        {
+            txtIspis.Clear();
+            CmbIzbor.SelectedIndex = 0;
+        }
+
+        private void btnPoruka_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Poruka", "Naslov");
+        }
+    }
+}
